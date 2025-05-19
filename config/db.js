87f11eda8b94c469fecb1f49a047895982c26232
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-05-20 00:37:10 +0530

    MongoDB database connection file

---
+++ config/db.js
@@ -0,0 +1,21 @@
+// config/db.js - File for connecting to MongoDB using Mongoose ODM
+
+const mongoose = require('mongoose');
+require('dotenv').config(); // Load env variables
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB Connected...');
+    } catch (err) {
+        console.error(err.message);
+
+        // Exit process with failure
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
