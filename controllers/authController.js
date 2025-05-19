1c5f0477f4f0dc1b4585a2a7e42c0ffdfd410c16
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-05-20 00:40:09 +0530

    Implemented auth controller file

---
+++ controllers/authController.js
@@ -0,0 +1,89 @@
+// controllers/authController.js - File for authentication controller
+
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { validationResult } = require('express-validator');
+
+// Generate JWT
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d', // Token expires in 30 days
+    });
+};
+
+// @desc    Register a new user
+// @route   POST /api/auth/signup
+// @access  Public
+const signupUser = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { username, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ message: 'User already exists with this email' });
+        }
+        user = await User.findOne({ username });
+        if (user) {
+            return res.status(400).json({ message: 'User already exists with this username' });
+        }
+
+        user = new User({
+            username,
+            email,
+            password,
+        });
+
+        await user.save();
+
+        res.status(201).json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+};
+
+// @desc    Authenticate user & get token
+// @route   POST /api/auth/login
+// @access  Public
+const loginUser = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (user && (await user.comparePassword(password))) {
+            res.json({
+                _id: user._id,
+                username: user.username,
+                email: user.email,
+                token: generateToken(user._id),
+            });
+        } else {
+            res.status(401).json({ message: 'Invalid email or password' });
+        }
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+};
+
+module.exports = {
+    signupUser,
+    loginUser,
+};
